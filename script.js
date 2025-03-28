550286c517daa3e548b935c2974395b11e0c35b9
JH-UX-DANTE <Harv0129@student.acitech.org>
2025-03-28 09:14:54 -0400

    Final Commit

--- script.js
+++ script.js
@@ -7,8 +7,30 @@ const choices = ['rock', 'paper', 'scissors', 'pizza', 'shredder'];
 
 buttons.forEach(button => {
     button.addEventListener('click', () => {
-    const playerChoice = button.id;
-    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
-    const winner = determineWinner(playerChoice, computerChoice);
-    resultElement.textContent = 'you chose ${playerChoice}, computer chose ${computerChoice}. ${winner}';
+        const playerChoice = button.id;
+        const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+        const winner = determineWinner(playerChoice, computerChoice);
+        console.log(winner);
+        resultElement.textContent = `you chose ${playerChoice}, computer chose ${computerChoice}. ${winner}`;
+    });
+});
+
+function determineWinner(player, computer) {
+    if (player == computer) {
+        return "It's a tie";
+     } else if (
+        (player == 'rock' && computer == 'scissors' ) || (player == 'scissors' && computer == 'paper') ||
+        (player == 'paper' && computer == 'rock') || (player == 'pizza' && computer == 'paper') ||
+        (player == 'shredder' && computer == 'paper') || (player == 'scissors' && computer == 'shredder')
+     ) {
+        return 'You win!';
+     } else {
+        return 'computer wins!';
+     }
+}
+
+if (winner == 'You win!') {
+    playerScore++;
+} else if (winner == 'Computer wins!') {
+    computerScore++;
 }
